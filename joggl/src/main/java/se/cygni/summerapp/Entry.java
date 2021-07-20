65387a3d8fe45b967171290520b4a3b77549cea1
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2021-07-21 00:30:33 +0200

    Creating a novel model of an entry

---
+++ joggl/src/main/java/se/cygni/summerapp/Entry.java
@@ -0,0 +1,127 @@
+package se.cygni.summerapp;
+
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+
+public class Entry
+{
+	private String Client;
+	private String Description;
+	private String Name;
+	private String Project;
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
+
+	public Entry(String client, String description,
+				String name, String project)
+	{
+		this.Client = client;
+		this.Description = description;
+		this.Name = name;
+		this.Project = project;
+		this.startTime = LocalDateTime.now();
+		this.endTime = null;
+	}
+
+	public String getName() 
+	{
+		return Name;
+	}
+
+	private void setName(String name) 
+	{
+		Name = name;
+	}
+
+	public String getClient() 
+	{
+		return Client;
+	}
+
+	private void setClient(String client) 
+	{
+		Client = client;
+	}
+
+	public String getDescription() 
+	{
+		return Description;
+	}
+
+	private void setDescription(String description) 
+	{
+		Description = description;
+	}
+
+	public String getProject() 
+	{
+		return Project;
+	}
+
+	private void setProject(String project) 
+	{
+		Project = project;
+	}
+
+	public LocalDateTime getStartTime() 
+	{
+		return startTime;
+	}
+
+	private void setStartTime(LocalDateTime startTime) 
+	{
+		this.startTime = startTime;
+	}
+
+	public LocalDateTime getEndTime() 
+	{
+		return endTime;
+	}
+
+	private void setEndTime(LocalDateTime endTime) 
+	{
+		this.endTime = endTime;
+	}
+
+	public LocalDateTime stopEntry()
+	{
+		LocalDateTime endTime = LocalDateTime.now();
+		this.setEndTime(endTime);
+		return endTime;
+	}
+
+	public void update(String property, String value)
+	{
+		switch (property)
+		{
+			case "Client": this.setClient(value);
+			break;
+
+			case "Project": this.setProject(value);
+			break;
+
+			case "Description": this.setDescription(value);
+			break;
+
+			case "Name": this.setName(value);
+			break;
+
+			default: throw new NoSuchElementException("No such property: \"" + property + "\"");
+		}
+	}
+
+	public void update(String property, LocalDateTime value)
+	{
+		switch (property)
+		{
+			case "StartTime": this.setStartTime(value);
+			break;
+
+			case "EndTime": this.setEndTime(value);
+			break;
+
+			default: throw new NoSuchElementException("No such property: \"" + property + "\"");
+		}
+	}
+}
+
---
+++ joggl/src/test/java/se/cygni/summerapp/EntryTest.java
@@ -0,0 +1,116 @@
+package se.cygni.summerapp;
+
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+/**
+ * Unit test for an Entry
+ */
+public class EntryTest
+{
+	String client = "The Client";
+	String name = "Test";
+	String project = "Development";
+	String description = "A bunch of tests";
+
+	Entry anEntry = new Entry(client, description, name, project);
+
+	@Test
+	public void createEntryTest()
+	{
+		assertTrue(anEntry.getClient() == client);
+		assertTrue(anEntry.getDescription() == description);
+		assertTrue(anEntry.getProject() == project);
+		assertTrue(anEntry.getName() == name);
+		assertTrue(LocalDateTime.now().isAfter(anEntry.getStartTime()));
+		assertTrue(anEntry.getEndTime() == null);
+	}
+
+	@Test
+	public void endEntryTest()
+	{
+		anEntry.stopEntry();
+		assertTrue(anEntry.getEndTime() != null);
+		assertTrue(anEntry.getEndTime().isAfter(anEntry.getStartTime()));
+		assertTrue(anEntry.getEndTime().isBefore(LocalDateTime.now()));
+	}
+
+	@Test
+	public void updateEntryTest()
+	{
+		String newClient = "Another Client";
+		String newName = "Updating test";
+		String newProject = "Development 2.0";
+		String newDescription = "Updating the entry";
+		LocalDateTime oldStartTime = anEntry.getStartTime();
+		LocalDateTime newStartTime = LocalDateTime.now();
+		LocalDateTime oldEndTime = anEntry.getEndTime();
+		LocalDateTime newEndTime = LocalDateTime.now();
+
+		try {
+		anEntry.update("Client", newClient);
+		}
+		catch (NoSuchElementException e) {
+			System.err.println(e.getMessage());
+			assertTrue(false);
+		}
+		assertTrue(anEntry.getClient() == newClient);
+		assertTrue(anEntry.getClient() != client);
+
+		try {
+		anEntry.update("Description", newDescription);
+		}
+		catch (NoSuchElementException e) {
+			System.err.println(e.getMessage());
+			assertTrue(false);
+		}
+		assertTrue(anEntry.getDescription() == newDescription);
+		assertTrue(anEntry.getDescription() != description);
+
+		try {
+		anEntry.update("Project", newProject);
+		}
+		catch (NoSuchElementException e) {
+			System.err.println(e.getMessage());
+			assertTrue(false);
+		}
+		assertTrue(anEntry.getProject() == newProject);
+		assertTrue(anEntry.getProject() != project);
+
+
+		try {
+		anEntry.update("Name", newName);
+		}
+		catch (NoSuchElementException e) {
+			System.err.println(e.getMessage());
+			assertTrue(false);
+		}
+		assertTrue(anEntry.getName() == newName);
+		assertTrue(anEntry.getName() != name);
+
+		try {
+		anEntry.update("StartTime", newStartTime);
+		}
+		catch (NoSuchElementException e) {
+			System.err.println(e.getMessage());
+			assertTrue(false);
+		}
+		assertTrue(anEntry.getStartTime() == newStartTime);
+		assertTrue(anEntry.getStartTime() != oldStartTime);
+
+		try {
+		anEntry.update("EndTime", newEndTime);
+		}
+		catch (NoSuchElementException e) {
+			System.err.println(e.getMessage());
+			assertTrue(false);
+		}
+		assertTrue(anEntry.getEndTime() == newEndTime);
+		assertTrue(anEntry.getEndTime() != oldEndTime);
+	}
+}
+
