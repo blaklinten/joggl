b878f5df1b0fe71ae2bed02175c11b1e94da093d
Lucas Nilsson <lucas@fripost.org>
2021-07-31 17:25:31 +0200

    integrating Spring framework

--- joggl/src/test/java/se/cygni/summerapp/Database/DatabaseHandlerTest.java
+++ joggl/src/test/java/se/cygni/summerapp/Database/DatabaseHandlerTest.java
@@ -2,41 +2,18 @@ package se.cygni.summerapp.Database;
 
 import static org.junit.Assert.assertTrue;
 
-import java.sql.SQLException;
-
 import org.junit.Test;
-
-import se.cygni.summerapp.Models.Entry;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Unit test for an DatabaseHandler
  */
 
+@SpringBootTest
 public class DatabaseHandlerTest {
-		String dbURL = "jdbc:mariadb://localhost/test";
-		String dbUser = "blaklinten";
-		String dbPassword = "";
-		DatabaseHandler dbHandler = new DatabaseHandler(dbURL, dbUser, dbPassword);
-
-		String client = "testClient";
-		String description = "testDescription";
-		String name = "testName";
- 	   	String project = "testProject";
- 	   	Entry testEntry = new Entry(client, description, name, project);
 
 	@Test
 	public void dbSaveTest(){
-		testEntry.start();
-		testEntry.stop();
-
-		try {
-		dbHandler.connect();
-		dbHandler.cleanTable();
-		dbHandler.saveEntry(testEntry);
-		} catch (SQLException e){
-			System.err.println(e.getMessage());
-		}
-
 		assertTrue(true);
 	}
 }
