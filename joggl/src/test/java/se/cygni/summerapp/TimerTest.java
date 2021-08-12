a62bae4f974553858e44cb8135a6455dd3675116
Lucas Nilsson <lucas@fripost.org>
2021-08-12 20:50:20 +0200

    Timer and related tests

---
+++ joggl/src/test/java/se/cygni/summerapp/TimerTest.java
@@ -0,0 +1,15 @@
+package se.cygni.summerapp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Unit test for the Timer component.
+ */
+@SpringBootTest
+public class TimerTest {
+}
+
