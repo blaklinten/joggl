277d269a4a9a5a72d2a8ecb94840d2981a102c84
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2021-08-28 23:30:49 +0200

    More Timer work, initial test

--- src/main/java/xyz/blaklinten/joggl/WebController.java
+++ src/main/java/xyz/blaklinten/joggl/WebController.java
@@ -1,16 +1,31 @@
 package xyz.blaklinten.joggl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import xyz.blaklinten.joggl.Models.Entry;
 
 @RestController
 public class WebController{
 
-	@PostMapping("/start-timer")
-	public String StartTimer(){
-		return "Started";
+	@Autowired private Timer timer;
+
+	@PostMapping(
+		value = "/start-timer",
+		consumes = {MediaType.APPLICATION_JSON_VALUE})
+
+	public String StartTimer(@RequestBody Entry entry){
+		try{
+		timer.start(entry);
+		}
+		catch (Timer.TimerAlreadyRunningException e){
+			return e.getMessage();
+		}
+		return "Started " + entry.getName() + " at time " + entry.getStartTimeAsString();
 	}
 
 	@GetMapping("/stop-timer")
--- src/test/java/xyz/blaklinten/joggl/TimerTest.java
+++ src/test/java/xyz/blaklinten/joggl/TimerTest.java
@@ -1,15 +1,48 @@
 package xyz.blaklinten.joggl;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import xyz.blaklinten.joggl.Models.Entry;
 
 /**
  * Unit test for the Timer component.
  */
 @SpringBootTest
 public class TimerTest {
+
+	@Autowired
+	private Timer timer;
+
+	@Test
+	public void startTimerTest(){
+
+		String client = "The Client";
+		String name = "Test";
+		String project = "Development";
+		String description = "A bunch of tests";
+
+		Entry anEntry = new Entry(name, client, project, description);
+
+		try{
+			timer.start(anEntry);
+		}
+		catch(Timer.TimerAlreadyRunningException e){
+			System.err.println(e.getMessage());
+		}
+		
+		assertThat(anEntry.getStartTime() != null);
+		assertThat(anEntry.getStartTime().isAfter(LocalDateTime.now()));
+
+		assertThrows(Timer.TimerAlreadyRunningException.class, () -> {
+			timer.start(anEntry);
+		});
+	}
 }
 
