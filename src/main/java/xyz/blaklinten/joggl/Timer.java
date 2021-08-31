baf811b9bb5d3f2888c902ad5a3d3145452c284b
Lucas Nilsson <lucas@fripost.org>
2021-08-31 14:27:01 +0200

    Timer functionality

--- src/main/java/xyz/blaklinten/joggl/Timer.java
+++ src/main/java/xyz/blaklinten/joggl/Timer.java
@@ -12,16 +12,18 @@ public class Timer {
 	public void start(Entry newEntry) throws TimerAlreadyRunningException{
 		if (entry != null) {
 			throw new TimerAlreadyRunningException("A timer is already running!");
+		} else {
+			entry = newEntry;
+			entry.start();
 		}
-
-		entry = newEntry;
-		entry.start();
 	}
 
-	public void stopTimer() throws NoActiveTimerException {
+	public void stop() throws NoActiveTimerException {
 		if (entry == null){
 			throw new NoActiveTimerException("No Timer to stop!");
 		}
+		entry.stop();
+		entry  = null;
 	}
 
 	public class NoActiveTimerException extends Exception {
--- src/test/java/xyz/blaklinten/joggl/TimerTest.java
+++ src/test/java/xyz/blaklinten/joggl/TimerTest.java
@@ -1,35 +1,36 @@
 package xyz.blaklinten.joggl;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import xyz.blaklinten.joggl.Models.Entry;
 
 /**
  * Unit test for the Timer component.
  */
-@SpringBootTest
 public class TimerTest {
 
-	@Autowired
-	private Timer timer;
+	private Timer timer = new Timer();
+	private Entry anEntry;
 
-	@Test
-	public void startTimerTest(){
-
+	@BeforeEach
+	public void init(){
 		String client = "The Client";
 		String name = "Test";
 		String project = "Development";
 		String description = "A bunch of tests";
 
-		Entry anEntry = new Entry(name, client, project, description);
+		anEntry = new Entry(name, client, project, description);
+	}
 
+	@Test
+	public void startTimerTest(){
 		try{
 			timer.start(anEntry);
 		}
@@ -37,11 +38,74 @@ public class TimerTest {
 			System.err.println(e.getMessage());
 		}
 		
-		assertThat(anEntry.getStartTime() != null);
-		assertThat(anEntry.getStartTime().isAfter(LocalDateTime.now()));
+		assertThat(anEntry.getStartTime()).isNotNull();
+		assertTrue(anEntry.getStartTime().isBefore(LocalDateTime.now()));
+	}
 
+	@Test
+	public void startTimerTwiceTest(){
+		try{
+			timer.start(anEntry);
+		}
+		catch(Timer.TimerAlreadyRunningException e){
+			System.err.println(e.getMessage());
+		}
+		
 		assertThrows(Timer.TimerAlreadyRunningException.class, () -> {
 			timer.start(anEntry);
+		});
+	}
+
+	@Test
+	public void stopRunningTimerTest(){
+		try{
+			timer.start(anEntry);
+		}
+		catch(Timer.TimerAlreadyRunningException e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			timer.stop();
+		}
+		catch(Timer.NoActiveTimerException e){
+			System.err.println(e.getMessage());
+		}
+
+		LocalDateTime endTime = anEntry.getEndTime();
+		LocalDateTime startTime = anEntry.getStartTime();
+
+		assertThat(endTime).isNotNull();
+		assertThat(startTime).isNotNull();
+		assertTrue(endTime.isAfter(startTime));
+	}
+
+	@Test
+	public void stopRunningTimerTwiceTest(){
+		try{
+			timer.start(anEntry);
+		}
+		catch(Timer.TimerAlreadyRunningException e){
+			System.err.println(e.getMessage());
+		}
+		
+		try{
+			timer.stop();
+		}
+		catch(Timer.NoActiveTimerException e){
+			System.err.println(e.getMessage());
+		}
+
+		assertThrows(Timer.NoActiveTimerException.class, () -> {
+			timer.stop();
+		});
+
+	}
+
+	@Test
+	public void stopNotRunningTimerTest(){
+		assertThrows(Timer.NoActiveTimerException.class, () -> {
+			timer.stop();
 		});
 	}
 }
