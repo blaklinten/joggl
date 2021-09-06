d15b03dc2bad9c0dcd2e2b42933d379b115b09c9
MeMyselfandI <30445004+blaklinten@users.noreply.github.com>
2021-09-06 14:18:44 +0200

    Adding option to sum in test script

--- examples/test_cli.sh
+++ examples/test_cli.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "%s\n" "What do you want to do?"
-select choise in "Start example entry" "Stop a running entry" "Get current status"; do
+select choise in "Start example entry" "Stop a running entry" "Get current status" "Sum by name"; do
 	case $choise in
 		Stop*)
 			curl localhost:8080/stop-timer
@@ -12,6 +12,10 @@ select choise in "Start example entry" "Stop a running entry" "Get current statu
 		Get*)
 			curl localhost:8080/get-status
 		;;
-		
+
+		Sum*)
+			read NAME
+			curl localhost:8080/sum-entries-by-name?name=$NAME
+		;;		
 	esac
 done
